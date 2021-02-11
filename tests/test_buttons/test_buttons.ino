955b222781e20169846d87c8d794ce032c3bf76d
Valentin Pelloin <valentinpelloin@gmail.com>
2021-02-11 20:58:24 +0100

    tests, new keypad layout

--- tests/test_buttons/test_buttons.ino
+++ tests/test_buttons/test_buttons.ino
@@ -4,10 +4,10 @@ const byte ROWS = 4;
 const byte COLS = 4;
 
 char keys[ROWS][COLS] = {
-	{'1','U','a','3'},
-	{'8','7','c','4'},
-	{'2','L','b','D'},
-	{'+','R','9','-'}
+	{1, 13, 10, 3},
+	{8, 7,  12, 4},
+	{2, 15, 11, 14},
+	{6, 16, 9,  5}
 };
 
 byte rowPins[ROWS] = {4, 7, 8, 9};
@@ -20,9 +20,20 @@ void setup(){
 }
   
 void loop(){
-	char key = keypad.getKey();
+	int key = (int) keypad.getKey();
 
 	if (key){
 		Serial.println(key);
 	}
+
+	int state = keypad.getState();
+	if (state == PRESSED) {
+		Serial.println("key pressed");
+	} else if (state == RELEASED ) {
+		Serial.println("key released");
+	} else if (state == HOLD) {
+		Serial.println("key hold");
+	}
+
+	delay(50);
 }
--- tests/test_vibration/test_vibration.ino
+++ tests/test_vibration/test_vibration.ino
@@ -7,6 +7,9 @@ int vibration_state = HIGH;
 int vibration_previous = LOW;
 int vibration_read;
 
+int i_loop = 0;
+int i_pwm = 0;
+
 void setup() {
 	pinMode(vibration_button_pin, INPUT_PULLUP);
 	pinMode(vibration_out1_pin, OUTPUT);
@@ -23,10 +26,25 @@ void loop() {
 	vibration_read = digitalRead(vibration_button_pin);
 	if (vibration_read == HIGH && vibration_previous == LOW) {
 		vibration_state = !vibration_state;
-		digitalWrite(vibration_led_pin, vibration_state);
+		digitalWrite(vibration_led_pin, !vibration_state);
 		Serial.println(vibration_state);
 	}
 	vibration_previous = vibration_read;
+
+	if (vibration_state == HIGH) {
+		analogWrite(vibration_out1_pin, i_pwm);
+		analogWrite(vibration_out2_pin, i_pwm);
+
+		if (i_loop > 10) {
+			i_pwm = (i_pwm + 1) % 255;
+			i_loop = 0;
+		} else {
+			i_loop += 1;
+		}
+	} else {
+		analogWrite(vibration_out1_pin, 0);
+		analogWrite(vibration_out2_pin, 0);
+	}
 
 	delay(2);
 }
