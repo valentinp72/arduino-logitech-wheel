1acd86205bab70580e558624a510375fdba74628
Valentin Pelloin <valentinpelloin@gmail.com>
2023-01-06 19:12:50 +0100

    updated with calibration

--- tests/test_force_feedback/test_force_feedback.ino
+++ tests/test_force_feedback/test_force_feedback.ino
@@ -19,6 +19,7 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {4, 7, 8, 9};
 byte colPins[COLS] = {10, 14, 15, 16};
 const int vibration_led_pin = 3; 
+const int vibration_btn_pin = 2;
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
@@ -36,8 +37,62 @@ Joystick_ Joystick(
 	false                  // steering
 );
 
-float EMA_a = 0.25;
-int EMA_s = 0;
+//float EMA_a = 0.25;
+//int EMA_s = 0;
+const int numReadings = 100;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+long int total = 0;                  // the running total
+long int average = 0;                // the average
+
+int vibration_toggle = 1;
+
+
+int wheel_min = 0, wheel_max = 0;
+int brake_min = 0, brake_max = 0;
+int accel_min = 0, accel_max = 0;
+
+void buttonWait(int pin) {
+  int buttonState = digitalRead(pin);
+  while(buttonState == HIGH) {
+    buttonState = digitalRead(pin);
+  }
+  while(buttonState == LOW) {
+    buttonState = digitalRead(pin);
+  }
+}
+
+void ledBlink(int pin) {
+  for (int i = 0 ; i < 5 ; i++) {
+    digitalWrite(pin, HIGH);
+    delay(50);
+    digitalWrite(pin, LOW);
+    delay(20);
+  }
+}
+
+void calibrate() {
+  
+  buttonWait(vibration_btn_pin);
+  ledBlink(vibration_led_pin);
+
+  brake_min = analogRead(brake_pin);
+  accel_min = analogRead(accel_pin);
+  wheel_min = analogRead(wheel_pin);
+
+  buttonWait(vibration_btn_pin);
+  ledBlink(vibration_led_pin);
+
+  brake_max = analogRead(brake_pin);
+  accel_max = analogRead(accel_pin);
+  wheel_max = analogRead(wheel_pin);
+
+  Serial.print(brake_min); Serial.println(brake_max);
+  Serial.print(accel_min); Serial.println(accel_max);
+  Serial.print(wheel_min); Serial.println(wheel_max);
+
+}
 
 void setup() {
 	Serial.begin(9600);
@@ -45,9 +100,14 @@ void setup() {
 	Joystick.setXAxisRange(-512, 511);
 	Joystick.setYAxisRange(0, 1023);
 	pinMode(vibration_led_pin, OUTPUT);
-	pinMode(2, INPUT_PULLUP);
+	pinMode(vibration_btn_pin, INPUT_PULLUP);
 
-	EMA_s = map(analogRead(wheel_pin), 0, 1023, -512, 511);
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+	//EMA_s = map(analogRead(wheel_pin), 0, 1023, -512, 511);
+
+  calibrate();
 }
 
 int old_wheel = 0;
@@ -57,6 +117,10 @@ int old_brake = 0;
 int last_key = 0;
 
 void loop() {
+  if(digitalRead(vibration_btn_pin) == LOW) {
+    calibrate();
+  }
+  
 	int key = (int) keypad.getKey();
 
 	if (key) {
@@ -69,28 +133,46 @@ void loop() {
 		Joystick.setButton(last_key, LOW);
 	}
 
-	int wheel = map(analogRead(wheel_pin), 0, 1023, -512, 511);
-	EMA_s = (EMA_a * wheel) + ((1 - EMA_a) * EMA_s);
-	wheel = EMA_s;
-	if (wheel != old_wheel) {
-		Joystick.setXAxis(wheel);
-		old_wheel = wheel;
-		Serial.print("-512,512,");
-		Serial.println(wheel);
+	// int wheel = map(analogRead(wheel_pin), 0, 1023, -512, 511);
+
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(wheel_pin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+ 
+	//EMA_s = (EMA_a * wheel) + ((1 - EMA_a) * EMA_s);
+	//wheel = EMA_s;
+	if (average != old_wheel) {
+		Joystick.setXAxis(map(average, wheel_min, wheel_max, -512, 511));
+		old_wheel = average;
+		// Serial.print("-512,512,");
+		// Serial.println(wheel);
 	}
 
 	int accel = analogRead(accel_pin);
 	if (accel != old_accel) {
-		Joystick.setYAxis(accel);
+		Joystick.setYAxis(map(accel, accel_min, accel_max, 0, 1023));
 		old_accel = accel;
 	}
 
 	int brake = analogRead(brake_pin);
 	if (brake != old_brake) {
-		Joystick.setZAxis(brake);
+		Joystick.setZAxis(map(brake, brake_min, brake_max, 0, 1023));
 		old_brake = brake;
 	}
 
-	delay(50);
+	delay(5);
 }
-
